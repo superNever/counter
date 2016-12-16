8ceeb93338eadfae6e932e66562d505ccf31a5ff
SuperNever <1021165525@qq.com>
2016-12-16 11:10:12 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {mobx,observable,computed} from 'mobx';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {observer} from 'mobx-react';
+ReactDOM.render(
+	<div>this is a counter</div>,document.querySelector('#mount'));
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+let env = process.env.WEBPACK_ENV;
+let plugins = [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new ExtractTextPlugin("[name][hash:8].css")
+]
+
+if(env==="dev"){
+  plugins.push(
+    new webpack.DllReferencePlugin({
+        context: __dirname,
+        manifest: require('./dist/react-manifest.json'),
+        name:'react_library'
+    }))
+}else{
+    plugins.push(
+      new webpack.optimize.OccurrenceOrderPlugin,
+      new webpack.optimize.UglifyJsPlugin({
+        mangle:   true,
+        compress: {
+            warnings: false, // Suppress uglification warnings
+        },
+        except: ['exports', 'require']
+      })
+    )
+}
+module.exports = {
+  entry: {
+    index:'./src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].js',
+    library: '[name]'
+  },
+  devtool:'eval', 
+  module:{
+    loaders: [
+      {
+        'test': /\.jsx?$/,
+        'loader': 'babel',
+        'exclude': ['node_modules',path.resolve(__dirname,'../node_modules/')]
+      },
+      {
+        test: /\.css/,
+        include:[path.resolve(__dirname,'/src/')],
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.less/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      }
+    ]
+  },
+  plugins:plugins
+};
